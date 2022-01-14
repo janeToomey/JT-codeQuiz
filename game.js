2640b84917c8df6a4bb990dbc5e751b2ae567661
janeToomey <j.toomey1027@gmail.com>
2022-01-13 22:28:14 -0800

    js additions

--- game.js
+++ game.js
@@ -1,8 +1,48 @@
+//initial variables
 const question = document.querySelector('#question');
 const choices = Array.from( document.querySelector('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progreeeBarFull = document.querySelector('#progressBArFull');
+//create input variables
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
 
-var currentQuestion = {}
-var acceptingAnswers = true
+//create an item to store questions-answers
+
+var questions = [
+    {
+        question: ' what is js?',
+        choiceA:'blah',
+        choiceB:'blah',
+        choiceC:'blah', 
+        choiceD:'blah',
+        answer: 'blah',
+    },
+    {
+        question: ' what is js?',
+        choiceA:'blah',
+        choiceB:'blah',
+        choiceC:'blah', 
+        choiceD:'blah',
+        answer: 'blah',
+    },
+];
+
+const scorePoint = 100
+const maxQuestion = 4
+
+startGame = () =>{
+    questionCOunter= 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNEwQuestion()
+}
+
+getNEwQuestion = () =>{
+    if(availableQuestions.length === 0 || questionsCounter> maxQuestion){
+        localStorage.setItem('mostRecentScore', score)
+    }
+}
