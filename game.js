c5e3e317a1b0c4238d618f36e0d6eaf4cae930e5
janeToomey <j.toomey1027@gmail.com>
2022-01-16 14:21:57 -0800

    re-thinking and editing

--- game.js
+++ game.js
@@ -35,14 +35,61 @@ const scorePoint = 100
 const maxQuestion = 4
 
 startGame = () =>{
-    questionCOunter= 0;
+    questionCounter= 0;
     score = 0;
     availableQuestions = [...questions];
-    getNEwQuestion()
+    getNewQuestion()
 }
 
-getNEwQuestion = () =>{
-    if(availableQuestions.length === 0 || questionsCounter> maxQuestion){
+getNewQuestion = () =>{
+    if(availableQuestions.length === 0 || questionCounter> maxQuestion){
         localStorage.setItem('mostRecentScore', score)
+        return window.location.assignment('/end.html')
     }
+    questionCounter++
+    progressTextText.innerText = `Question ${questionCounter} of ${maxQuestion}`
+    progressBarFull.style.width =`${(questionCounter/maxQuestion) * 100}%`
+    const questionIndex = Math.floor(Math.random()*availablequestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice =>{
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion ['choice'+ number]
+    })
+
+    availableQuestions.splice(questionIndex, 1)
+
+    acceptingAnswers = true
 }
+
+choices.forEach(choice =>{
+    choice.addEventListener('click', e =>{
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = falseconst selectedChoice = e.target
+        const selectedAnser - selectedChoice.dataset ['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct'){
+            incrementScore(scorePoints)
+
+
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(()=>{
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+incrementScore = nim =>{
+    score +=num
+    scoreText.innerText = score
+
+}
+startGame()
