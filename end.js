af7d0d52e6cfe51e32b85931e3ea3b7c99ace95f
janeToomey <j.toomey1027@gmail.com>
2022-01-18 23:14:57 -0800

    some js changes

--- end.js
+++ end.js
@@ -4,9 +4,9 @@ const saveScoreBtn = document.querySelector('#savedScoreBtn')
 const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 //adding highscore to local storage
-const highScore = JSON.parse(localStorage.getItem('highScore'))|| []
+const highScores = JSON.parse(localStorage.getItem('highScore'))|| []
 //setting max high score
-const MAX_HIGH_SCORE = 6
+const MAX_HIGH_SCORE = 5
 //making sure current score shows up
 finalScore.innerText = mostRecentScore
 //when key pressed, save button enabled
@@ -20,21 +20,21 @@ saveHighScore = e =>{
     e.preventDefault()
     //variable to create user/score object
     const score = {
-        score: mostRecentScore,
+        score:Math.floor(Math.random()* 100),
         name: username.value
     }
 
-    highScore.push(score)
-    
-    highScore.sort((a,b)=>{
+    highScores.push(score)
+    //sorts the score
+    highScores.sort((a,b)=>{
         return b.score - a.score
     })
-
-    highScore.splice(6)
-
-    localStorage.setItem('highScore', JSON.stringify(highScore))
+    //cuts off high scores after 5 top scores reached
+    highScores.splice(5)
+     localStorage.setItem('highScores', JSON.stringify(highScores));
+   window.location.assign('index2.html');
     
-    window.location.assign('index2.html')
+    
 
     
 }
--- highscores.js
+++ highscores.js
@@ -3,7 +3,10 @@
 const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem('highScore'))||[]
 //logs the high scores of the players
-highScoresList.innerHTML =
-highScores.map(score =>{
-    return `<li class = "high-score">${score.name} - ${score.score}</li>`
-}).join('')
+highScoresList.innerHtml = highScores
+.map(score =>{
+    return `<li class = "high-score">${score.name} - ${score.score}</li>`;
+})
+.join('');
+
+
